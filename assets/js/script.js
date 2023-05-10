6a3243d38c8106bf1b53aca89f41deb3788a2ddf
Saeeda <saeedabatool39@gmail.com>
2023-05-09 20:38:01 -0400

    fetched API's and data for meals

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,34 @@
 
 
 //Saeeda section
+const mealElement = document.getElementById('generatedMeal')
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+  .then(response => response.json())
+  .then(data => {
+    const meal = data.meals[0];
+    const mealName = meal.strMeal;
+    const mealPicture = meal.strMealThumb;
+    const mealSource = meal.strSource;
+    // console.log(`Meal Name: ${mealName}`);
+    // console.log(`Meal Picture: ${mealPicture}`);
+    // console.log(`Meal Source: ${mealSource}`);
+    mealElement.innerHTML = `
+     <h2>${mealName}</h2>
+     <img src="${mealPicture}" alt="${mealName}" width="400" height="300">
+     <p>Source: <a href="${mealSource}" target="_blank">${mealSource}</a></p>
+  `;
+  })
+  .catch(error => console.error(error));
 
 
-
-
-
-
-
-
+// async function getData()
+// {
+//     let response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+//     let data = response.json()
+//      return data;
+// }
+// getData()
+// .then(data => console.log(data));
 
 
 
