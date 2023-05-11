32edc4856ada9eaf2b66723cb9859e6e8ab53f43
vlada_caban <vladaonly@gmail.com>
2023-05-11 18:54:41 -0400

    added modal to confirm changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,13 @@ $(function () {
   let movieTitle;
   let localStorageData = JSON.parse(localStorage.getItem("movieMealData"));
 
-  const saveBtn = $("<button>").addClass("btn btn-success m-2").text("Save");
+  const saveBtn = $("<button>")
+    .addClass("btn btn-success m-2")
+    .text("Save")
+    .attr("type", "button")
+    .attr("data-bs-toggle", "modal")
+    .attr("data-bs-target", "#saveConfirmation");
+
   const regenerateBtn = $("<button>")
     .addClass("btn btn-secondary m-2")
     .text("Re-Generate");
@@ -143,7 +149,11 @@ $(function () {
     pickMeal();
     $("#generatedPage").append(saveBtn);
     saveBtn.on("click", function () {
-      setLocalStorage();
+      $("#confirmSave").on("click", function() {
+        setLocalStorage();
+        // $("#saveConfirmation").attr("data-bs-dismiss", "modal");
+        $("#saveConfirmation").modal("toggle");
+      })
     });
   });
 
